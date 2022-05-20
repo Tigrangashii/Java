5a17a94781fe73e0ab888d50af2d1273f1825d81
Tigrangashii <105950508+Tigrangashii@users.noreply.github.com>
2022-05-20 23:51:34 +0200

    Update Asistenti.java

--- Asistenti.java
+++ Asistenti.java
@@ -1,22 +1,12 @@
-/*
-1.3.Krijoni klasën Asistenti që është një Mesimdhenesi që ka angazhimin “Ushtrime”dhe ka atributin
-shtesë: llojiUshtimreve (p.sh. Numerike, Laboratorike, etj.)
-a) Ofroni metodat get dhe set për ato atribute që ju e shihni të nevojshme.
-b) Ofroni metodën që reprezenton në String një objekt të klasës Asistenti në formatin:
-Asistenti <emri> : < vitiLindjes > mban <angazhimi> <llojiUshtrimeve>
- */
 
 public class Asistenti extends Mesimdhenesi {
 
     private String llojiUshtrimeve;
-
-    ////Asistenti a1 = new Asistenti("Tigran Gashi", 2000, "Ushtrime", "numerike");
     public Asistenti(String e,int vl,String llU){
         super(e, vl, "Ushtrime");
         llojiUshtrimeve=llU;
     }
 
-//a) Ofroni metodat get dhe set për ato atribute që ju e shihni të nevojshme.
     public String getLlojiUshtrimeve(){
         return llojiUshtrimeve;
     }
@@ -25,8 +15,6 @@ public class Asistenti extends Mesimdhenesi {
     }
 
 
-/*b) Ofroni metodën që reprezenton në String një objekt të klasës Asistenti në formatin:
-        Asistenti <emri> : < vitiLindjes > mban <angazhimi> <llojiUshtrimeve>  */
     @Override
     public String toString(){
          return "Asistenti" + super.toString() + llojiUshtrimeve;
